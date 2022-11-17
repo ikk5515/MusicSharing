000837fc9fdf21d8d91f36d118cfd5da7d22e980
ikk5515 <ikk5515@naver.com>
2022-11-17 22:30:14 +0900

    기본 이미지 파일 구성

---
+++ src/main/java/music/korMusic.java
@@ -0,0 +1,55 @@
+package music;
+
+public class korMusic {
+	private int korno;
+	private String korTitle;
+	private String korSinger;
+	private String korYoutube;
+	private String korKind;
+	private String UserNick;
+	private String kordate;
+	
+	
+	public String getUserNick() {
+		return UserNick;
+	}
+	public void setUserNick(String userNick) {
+		UserNick = userNick;
+	}
+	public int getKorno() {
+		return korno;
+	}
+	public void setKorno(int korno) {
+		this.korno = korno;
+	}
+	public String getKorTitle() {
+		return korTitle;
+	}
+	public void setKorTitle(String korTitle) {
+		this.korTitle = korTitle;
+	}
+	public String getKorSinger() {
+		return korSinger;
+	}
+	public void setKorSinger(String korSinger) {
+		this.korSinger = korSinger;
+	}
+	public String getKorYoutube() {
+		return korYoutube;
+	}
+	public void setKorYoutube(String korYoutube) {
+		this.korYoutube = korYoutube;
+	}
+	public String getKorKind() {
+		return korKind;
+	}
+	public void setKorKind(String korKind) {
+		this.korKind = korKind;
+	}
+	public String getKordate() {
+		return kordate;
+	}
+	public void setKordate(String kordate) {
+		this.kordate = kordate;
+	} 
+}
