f83a24c2828cc9ac60e57e496c17c42d01009449
ikk5515 <ikk5515@naver.com>
2022-11-19 01:51:04 +0900

    질문게시판 class 파일

---
+++ src/main/java/music/contentAll.java
@@ -0,0 +1,50 @@
+package music;
+
+public class contentAll {
+	private int contentNo;
+	private String contentTitle;
+	private String contentWrite;
+	private String UserNick;
+	private String contentDate;
+
+	public int getContentNo() {
+		return contentNo;
+	}
+
+	public void setContentNo(int contentNo) {
+		this.contentNo = contentNo;
+	}
+
+	public String getContentTitle() {
+		return contentTitle;
+	}
+
+	public void setContentTitle(String contentTitle) {
+		this.contentTitle = contentTitle;
+	}
+
+	public String getContentWrite() {
+		return contentWrite;
+	}
+
+	public void setContentWrite(String contentWrite) {
+		this.contentWrite = contentWrite;
+	}
+
+	public String getUserNick() {
+		return UserNick;
+	}
+
+	public void setUserNick(String userNick) {
+		UserNick = userNick;
+	}
+
+	public String getContentDate() {
+		return contentDate;
+	}
+
+	public void setContentDate(String contentDate) {
+		this.contentDate = contentDate;
+	}
+
+}
