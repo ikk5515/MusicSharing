aca92766bc83c0c667f1ee3165a9ec1404c44096
ikk5515 <ikk5515@naver.com>
2022-11-20 04:45:40 +0900

    user 질문 class파일 추가

---
+++ src/main/java/music/comment.java
@@ -0,0 +1,42 @@
+package music;
+
+public class comment {
+	private int contentNo;
+	private int commentNo;
+	private String commentWrite;
+	private String userNick;
+	private String commentDate;
+	
+	public int getContentNo() {
+		return contentNo;
+	}
+	public void setContentNo(int contentNo) {
+		this.contentNo = contentNo;
+	}
+	public int getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+	public String getCommentWrite() {
+		return commentWrite;
+	}
+	public void setCommentWrite(String commentWrite) {
+		this.commentWrite = commentWrite;
+	}
+	public String getUserNick() {
+		return userNick;
+	}
+	public void setUserNick(String userNick) {
+		this.userNick = userNick;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
+	}
+
+	
+}
